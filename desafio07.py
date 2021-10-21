534df83e8877e78914dda06e4253151299a437ef
gabriell.oliveira <79490046+bieloliveira8@users.noreply.github.com>
2021-10-21 13:00:41 -0300

    quinto commit

---
+++ desafio07.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digita a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = n1+n2
+print(f'A média do aluno somando {n1} e {n2} é {media} \nContinue se esforçando!')
